23c3dbc465fbebcc9a53c291f3c6e64f6546cd24
marvinick <marviluw@gmail.com>
2015-04-28 16:20:08 -0500

    finished movie night

--- movie.rb
+++ movie.rb
@@ -11,16 +11,40 @@ case choice
 when "add"
     puts "Add the movie!"
     title = gets.chomp
-    puts "What's the rating?"
-    rating = gets.chomp
-    movies[title.to_sym] = rating.to_i
-    puts "#{title} has been added with a rating of #{rating}."
+    if movies[title.to_sym].nil?
+        puts "What's the rating?"
+        rating = gets.chomp
+        movies[title.to_sym] = rating.to_i
+        puts "#{title} has been added."
+    else
+        puts "That movie is already there! The rating is #{movies[title.to_sym]}"
+    end
 when "update"
-    puts "Updated!"
+    puts "What's the title?"
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+    puts "The title does not exist!"
+    else
+        puts "set the new rating"
+        rating = gets.chomp.to_i
+        movies[title.to_sym] = rating
+        puts "The new rating is #{movies[title.to_sym]}"
+    end
+
 when "display"
-    puts "Movies!"
+    movies.each do |title, rating| puts "#{title}: #{rating}"
+end
+
 when "delete"
-    puts "Deleted!"
+    puts "Enter the title:"
+    title = gets.chomp.to_sym
+    if movies[title].nil?
+        puts "There is no movie with that title!"
+    else
+        movies.delete(title)
+    puts "It's been deleted"
+    end
+
 else
     puts "Error!"
 end
